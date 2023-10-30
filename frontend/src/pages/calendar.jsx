a3e0709535c13d99e295d83ae75a210662b49f43
DerenB <dbozerprogramming@gmail.com>
2023-10-30 10:20:10 -0400

    calendar testing

--- frontend/src/pages/calendar.jsx
+++ frontend/src/pages/calendar.jsx
@@ -1,38 +1,63 @@
-import React, { useState } from 'react';
-import Calendar from 'react-calendar';
-import "react-calendar/dist/Calendar.css";
+import React, { Component } from 'react';
+import CalendarDays from '../components/calendarDays';
+import '../styles/calendar.css';
 
-const calendar = () => {
-  const [date, changeDate] = useState(new Date());
+export default class Calendar extends Component {
+  constructor() {
+    super();
 
-  function changeValue(val) {
-    changeDate(val);
- }
+    this.weekdays = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+    this.months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
-  const test_date = [
-    {
-      "id": 2,
-      "subject": "Test Subject",
-      "location": "Test Location",
-      "starttime": "2023-11-24T12:00:00.000Z",
-      "endtime": "2023-11-24T16:00:00.000Z",
-      "categorycolor": "#0000ff"
-    },
-    {
-      "id": 1,
-      "subject": "Test Subject",
-      "location": "Test Location",
-      "starttime": "2023-11-24T12:00:00.000Z",
-      "endtime": "2023-11-24T16:00:00.000Z",
-      "categorycolor": "#0000ff"
+    this.state = {
+      currentDay: new Date()
+    }
   }
-  ]
 
-  return (
-    <div>
-      <Calendar onChange={changeValue} value={date} />
-    </div>
-  )
+  changeCurrentDay = (day) => {
+    this.setState({ currentDay: new Date(day.year, day.month, day.number) });
+  }
+
+  nextDay = () => {
+    this.setState({ currentDay: new Date(this.state.currentDay.setDate(this.state.currentDay.getDate() + 1)) });
+  }
+
+  previousDay = () => {
+    this.setState({ currentDay: new Date(this.state.currentDay.setDate(this.state.currentDay.getDate() - 1)) });
+  }
+
+  render() {
+    return (
+      <div className="calendar">
+        <div className="calendar-header">
+          <div className="title">
+            <h2>{this.months[this.state.currentDay.getMonth()]} {this.state.currentDay.getFullYear()}</h2>
+          </div>
+          <div className="tools">
+            <button onClick={this.previousDay}>
+              <span className="material-icons">
+                arrow_back
+              </span>
+            </button>
+            <p>{this.months[this.state.currentDay.getMonth()].substring(0, 3)} {this.state.currentDay.getDate()}</p>
+            <button onClick={this.nextDay}>
+              <span className="material-icons">
+                arrow_forward
+              </span>
+            </button>
+          </div>
+        </div>
+        <div className="calendar-body">
+          <div className="table-header">
+            {
+              this.weekdays.map((weekday) => {
+                return <div className="weekday"><p>{weekday}</p></div>
+              })
+            }
+          </div>
+          <CalendarDays day={this.state.currentDay} changeCurrentDay={this.changeCurrentDay} />
+        </div>
+      </div>
+    )
+  }
 }
-
-export default calendar
