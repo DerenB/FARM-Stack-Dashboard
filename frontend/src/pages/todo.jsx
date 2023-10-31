132b3600083598855258eafe69d27a1271ad0942
DerenB <dbozerprogramming@gmail.com>
2023-10-31 19:45:53 -0400

    Updating backend navigation

--- frontend/src/pages/todo.jsx
+++ frontend/src/pages/todo.jsx
@@ -7,7 +7,7 @@ function todo() {
   const [desc, setDesc] = useState();
 
   const addTodoHandler = () => {
-    axios.post('http://127.0.0.1:8000/todo', {
+    axios.post('https://grim-corpse-10162-99b7a1fba23f.herokuapp.com/todo', {
       'title': title,
       'desc': desc
     })
